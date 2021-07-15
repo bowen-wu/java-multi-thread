3ff54ee5cb683c046b97de9b42d8325cc934f0d6
bowen-wu <13793248162@163.com>
2021-07-15 20:56:24 +0800

    feat: delete lock in parallelStream()

--- src/main/java/com/github/hcsp/multithread/MultiThreadWordCount6.java
+++ src/main/java/com/github/hcsp/multithread/MultiThreadWordCount6.java
@@ -7,14 +7,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class MultiThreadWordCount6 {
     // 使用threadNum个线程，并发统计文件中各单词的数量
-    private static final Object lock = new Object();
     private static final Map<String, Integer> resultMap = new ConcurrentHashMap<>();
 
     public static Map<String, Integer> count(int threadNum, List<File> files) {
         files.parallelStream().forEach(file -> {
-            synchronized (lock) {
-                ProcessFile.convertWordsInFileToMap(file, resultMap);
-            }
+            ProcessFile.convertWordsInFileToMap(file, resultMap);
         });
 
         return resultMap;
